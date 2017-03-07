75f0b7ebdb3c2b2f8fcf23b9d5944507287f3356
tafarii <thuntermckenzie@gmail.com>
2017-03-07 14:14:55 -0500

    Creates shopping cart. Connects products to cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,9 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails5 for more book information.
 #---
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
 #
@@ -15,5 +18,14 @@ class Product < ApplicationRecord
     with:    %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+  private
+  #ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+        end
+    end
 end
 
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -56,6 +56,16 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   # ...
 
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+        delete product_url(products(:two))
+    end
+    assert_redirected_to products_url
+  end
+
+
+
+
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
